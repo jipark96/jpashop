5b6134132ce3e41bdfdcbdf89b9e7b81cc59c565
jipark96 <wlgus996@naver.com>
2023-01-08 04:46:01 +0900

    주문 상태 목록 검색 취소(목록 수정중)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,18 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
-
     private final EntityManager em;
-
     public void save(Order order) {
         em.persist(order);
     }
-
     public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
